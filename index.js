df6cdc412245f0eb3808ffd4eeeaef3c6fb99962
Rheinard Korf <rheinard@thekorfs.com>
2019-08-05 23:57:38 +1000

    Add 'mermaidOptions' as plugin options
    `mermaidOptions` can now be used to specify additional mermaid
    initialization options.

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const path = require('path');
 const visit = require('unist-util-visit');
 const puppeteer = require('puppeteer');
 
-async function render(browser, definition, theme, viewport) {
+async function render(browser, definition, theme, viewport, mermaidOptions) {
     const page = await browser.newPage();
     page.setViewport(viewport);
     await page.goto(`file://${path.join(__dirname, 'render.html')}`);
     await page.addScriptTag({
         path:  require.resolve('mermaid/dist/mermaid.min.js')
     });
-    return await page.$eval('#container', (container, definition, theme) => {
+    return await page.$eval('#container', (container, definition, theme, mermaidOptions) => {
         container.innerHTML = `<div class="mermaid">${definition}</div>`;
 
         try {
-            window.mermaid.initialize({theme: theme});
+            window.mermaid.initialize({
+                ...mermaidOptions,
+                theme
+            });
             window.mermaid.init();
             return container.innerHTML;
         } catch (e) {
             return `${e}`
         }
-    }, definition, theme);
+    }, definition, theme, mermaidOptions);
 }
 
 function mermaidNodes(markdownAST, language) {
@@ -37,6 +40,7 @@ module.exports = async ({markdownAST},
                             language = 'mermaid',
                             theme = 'default',
                             viewport = {height: 200, width: 200},
+                            mermaidOptions = {}
                         }) => {
 
     // Check if there is a match before launching anything
@@ -51,7 +55,7 @@ module.exports = async ({markdownAST},
 
     await Promise.all(nodes.map(async node => {
         node.type = 'html';
-        node.value = await render(browser, node.value, theme, viewport);
+        node.value = await render(browser, node.value, theme, viewport, mermaidOptions);
     }));
     browser.close();
 };
