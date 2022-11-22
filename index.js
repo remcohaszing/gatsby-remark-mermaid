b4044c3af90510cfa38415fdc644c0cdbdcb3616
Remco Haszing <remcohaszing@gmail.com>
2022-11-22 17:58:15 +0100

    Pass a vfile to the transformer
    It’s not used by remark-mermaidjs (yet), but unified plugins do expect
    it.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-module.exports = async ({ markdownAST }, options) => {
+module.exports = async ({ markdownAST, markdownNode }, options) => {
+  const { VFile } = await import('vfile')
   const { default: plugin } = await import('remark-mermaidjs')
+
+  const vfile = new VFile({
+    value: markdownNode.rawMarkdownBody,
+    path: markdownNode.fileAbsolutePath
+  })
   const transformer = plugin(options)
-  return transformer(markdownAST)
+  return transformer(markdownAST, vfile)
 }
